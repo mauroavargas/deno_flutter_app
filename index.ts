6d2de04d6eb1fb74ec7f3e3fbf97b85a140dd5a0
mauroavargas <mauro.vargas@catodigo.co>
2020-05-26 00:00:00 -0500

    Add quotes and get route in browser

--- index.ts
+++ index.ts
@@ -1,12 +1,20 @@
 import { Application, Router } from "https://deno.land/x/denotrain@v0.5.0/mod.ts";
+import { quotes } from './quotes.ts';
 
-const app = new Application({});
+const app = new Application({ hostname: '192.168.1.3' });
 
 const router = new Router();
 
 
 app.get('/', (ctx)=>{
     return 'Hello Mauro from Deno!!';
+})
+
+app.use('/api', router);
+
+
+router.get('/quotes', (ctx)=>{
+    return {'quotes' : quotes};
 })
 
 await app.run();
